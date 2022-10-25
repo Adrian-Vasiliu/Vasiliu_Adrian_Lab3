5b09c7f05198a9c031b310c4c134cadc794d6464
Adrian Vasiliu <adrian.vasiliu@ontegra.com>
2022-10-25 20:26:09 +0300

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace Vasiliu_Adrian_Lab2.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int? AuthorId { get; set; }
+        public Author? Author { get; set; }
+        public decimal Price { get; set; }
+        public ICollection<Order>? Orders { get; set; }
+    }
+}
